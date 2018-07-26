14ec8744da5132354c5d46b7643f5ee1b7f052dd
Korzon,Conrad <CKORZON@travelers.com>
2018-07-26 08:43:57 -0400

    footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EducationComponent } from './education/education.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule, routes } from './app-routing.module';
 import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { SkillsComponent } from './skills/skills.component';
     EducationComponent,
     AboutMeComponent,
     SkillsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
